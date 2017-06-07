bdf8c1aa6bce1643ecd3a2667739112aa930bf3d
zengyq <zengyunqing@froevercross.com>
2017-06-07 23:28:37 +0800

    提交测试

---
+++ src/com/OnlineTest/test/Test.java
@@ -0,0 +1,19 @@
+/**
+ * Test.java 2017年6月7日
+ */
+package com.OnlineTest.test;
+
+/**
+ * <p>
+ * <b>Test</b> is
+ * </p>
+ *
+ * @since 2017年6月7日
+ * @author zengyq
+ * @version $Id$
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("hello boy");
+	}
+}
