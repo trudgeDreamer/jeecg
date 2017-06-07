a787d9f3653872853e3fcee850b1b904c2adfed9
zengyq <zengyunqing@froevercross.com>
2017-06-07 23:54:36 +0800

    git 新增测试for eclipse

---
+++ src/com/test/NewTestFile.java
@@ -0,0 +1,19 @@
+/**
+ * NewTestFile.java 2017年6月7日
+ */
+package com.test;
+
+/**
+ * <p>
+ * <b>NewTestFile</b> is
+ * </p>
+ *
+ * @since 2017年6月7日
+ * @author zengyq
+ * @version $Id$
+ */
+public class NewTestFile {
+	public static void main(String[] args) {
+		System.out.println("gitFile add Test for eclipse");
+	}
+}
